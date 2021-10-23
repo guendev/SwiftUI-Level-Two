3a6d6696c4d5b4e63096bd2d88b31c0890fd3ff2
yuan <dnstylish@gmail.com>
2021-10-23 13:25:10 +0700

    Initial Commit

---
+++ Shared/SwiftUI_Level_TwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Level_TwoApp.swift
+//  Shared
+//
+//  Created by Yuan on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Level_TwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
